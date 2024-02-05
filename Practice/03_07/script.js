17805984311fa418529d5f0a9162a3c4b5f3d522
remnantsofred <remnantsofred@gmail.com>
2024-02-05 08:56:23 -0800

    Practice: building objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,40 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const desk = {
+  name: "Uplift desk",
+  width: "3 ft",
+  length: "2 ft",
+  height: 4,
+  setHeight: function (newHeight) {
+    this.height = newHeight;
+  },
+  monitors: {
+    left: true,
+    right: true,
+  },
+  updateMonitors: function (side, status) {
+    this.monitors[side] = status;
+    console.log(`${side} status`, this.monitors[side])
+  }
+}
+
+const iPhone = {
+  name: "Daphne's cell",
+  size: "regular",
+  battery: 98,
+  focusMode: true,
+  toggleFocusMode: function () {
+    this.focusMode = !this.focusMode;
+  },
+  chargePhone: function() {
+    this.battery = 100;
+  }
+}
+
+
+console.log("desk object:", desk)
+console.log("update monitors", desk.updateMonitors("left", false))
+console.log("updated desk:", desk)
+console.log('iPhone', iPhone)
